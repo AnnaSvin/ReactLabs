f6444f40eb7f0d0a48d6570f3f63253836081bd7
Anna Svintsitska <ipz233_saye@student.ztu.edu.ua>
2025-10-08 22:26:08 +0300

    Created ResultPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import "./index.css";
 import StartPage from "./pages/StartPage";
 import GamePage from "./pages/GamePage";
+import ResultPage from "./pages/ResultPage";
 
 function App() {
   const [page, setPage] = useState<"start" | "game" | "result">("start");
+  const [result, setResult] = useState<string | null>(null);
 
   return (
     <div className="app">
@@ -12,9 +14,13 @@ function App() {
       {page === "game" && (
         <GamePage
           onGameEnd={(res: string) => {
+            setResult(res);
             setPage("result");
           }}
         />
+      )}
+      {page === "result" && (
+        <ResultPage result={result} onRestart={() => setPage("start")} />
       )}
     </div>
   );
