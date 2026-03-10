dc18330a1a66d1f2b34167b2d7d892486fcf2a1c
Anna Svintsitska <ipz233_saye@student.ztu.edu.ua>
2026-03-10 17:18:41 +0200

    Added game settings types and local storage context.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import App from "./App";
+import { GameSettingsProvider } from "./context/GameSettingsContext";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GameSettingsProvider>
+      <App />
+    </GameSettingsProvider>
+  </StrictMode>
+);
