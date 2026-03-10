fe9ace741e9f933f6b3bd0995078fad161dfbcb3
Anna Svintsitska <ipz233_saye@student.ztu.edu.ua>
2026-03-10 16:32:53 +0200

    Added game types and helper functions for tic-tac-toe logic.

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,32 @@
-export function createEmptyBoard() {
+import type { BoardState, GameResult } from "../types/game.types";
+
+export function createEmptyBoard(): BoardState {
   return Array(9).fill(null);
+}
+
+export function checkWinner(board: BoardState): GameResult {
+  const winningLines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (const [a,b,c] of winningLines) {
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+
+  if (board.every(cell => cell !== null)) {
+    return "draw";
+  }
+
+  return null;
 }
